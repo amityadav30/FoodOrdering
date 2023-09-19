8052d4e87233cfeff1dd5fc56d7cbe154eec4087
Amitkumar Radheshyam Yadav <yadav.am@northeastern.edu>
2023-09-18 21:42:45 -0400

    Updates

--- src/components/utils/useAllRestaurant.js
+++ src/components/utils/useAllRestaurant.js
@@ -7,21 +7,34 @@ const useAllRestaurant = () => {
     fetchData();
   }, []);
 
-  const fetchData = async () => {
-    const fetchD = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
-    );
-    const json = await fetchD.json();
-    const data = json?.data?.cards?.find(
-      (item) =>
-        item?.card?.card?.gridElements?.infoWithStyle?.restaurants != undefined
-    );
-    const restaurant =
-      data?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-    const restaurantList = restaurant || [];
+  // const fetchData = async () => {
+  //   const fetchD = await fetch(
+  //     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5204303&lng=73.8567437&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING"
+  //   );
+  //   const json = await fetchD.json();
+  //   const data = json?.data?.cards?.find(
+  //     (item) =>
+  //       item?.card?.card?.gridElements?.infoWithStyle?.restaurants != undefined
+  //   );
+  //   const restaurant =
+  //     data?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+  //   const restaurantList = restaurant || [];
 
-    console.log("DATAAA ", restaurantList);
-    setRestaurant(restaurantList); //Initially assigning both with all the data fetched and
+  //   console.log("DATAAA ", restaurantList);
+  //   setRestaurant(restaurantList); //Initially assigning both with all the data fetched and
+  // };
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "http://food-ordering-server-amityadav30.vercel.app/api/all-restaurants"
+      ); // Update this route
+      const data = await response.json();
+      console.log("WWWWW", data);
+      setRestaurant(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   return restaurant;
--- src/components/utils/useRestaurant.js
+++ src/components/utils/useRestaurant.js
@@ -8,14 +8,26 @@ const useRestaurant = (resId) => {
     getRestaurantInfo(resId);
   }, []);
 
+  // async function getRestaurantInfo(resId) {
+  //   const data = await fetch(
+  //     `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=${resId}&submitAction=ENTER`
+  //   );
+  //   const json = await data.json();
+  //   setRestaurant(json?.data);
+  //   // console.log("Data ", data);
+  //   // console.log("Json ", json);
+  // }
+
   async function getRestaurantInfo(resId) {
-    const data = await fetch(
-      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=${resId}&submitAction=ENTER`
-    );
-    const json = await data.json();
-    setRestaurant(json?.data);
-    // console.log("Data ", data);
-    // console.log("Json ", json);
+    try {
+      const response = await fetch(
+        `http://food-ordering-server-amityadav30.vercel.app/api/restaurant-menu/${resId}`
+      ); // Update this route
+      const data = await response.json();
+      setRestaurant(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }
 
   return restaurant;
